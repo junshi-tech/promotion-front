3dae0a4aac41d3286a42fab87f9fc5c6b1e9ed7a
junshi2019 <junshi2019@qq.com>
2019-06-14 15:54:56 +0800

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git pull upstream master && echo 'Update app successed.'
+git pull upstream master && \cp -fr dist/* ../promotion-backstage/public/ui/dist/ && echo 'Update app successed.'
